05bed1062c5a4fb68010012e17b7de09d2ab9710
kilpelainenj <juhomatti.kilpelainen@gmail.com>
2026-01-02 17:35:46 +0100

    Add initial AST for statements

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -1,5 +1,7 @@
 package com.craftinginterpreters.lox;
 
+import java.util.List;
+
 class RuntimeError extends RuntimeException {
     final Token token;
 
@@ -9,17 +11,20 @@ class RuntimeError extends RuntimeException {
     }
 }
 
-class Interpreter implements Expr.Visitor<Object> {
+class Interpreter implements Expr.Visitor<Object>,
+                            Stmt.Visitor<Void> {
     
 
-    void interpret(Expr expression) {
+    void interpret(List<Stmt> statements) {
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
         } catch (RuntimeError error) {
-            Lox.runtimeError(error);
+                Lox.runtimeError(error);
         }
     }
+    
 
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
@@ -135,5 +140,22 @@ class Interpreter implements Expr.Visitor<Object> {
 
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
     }
 }
