39bdda51db84971a9678dc7401809b3ea5c0542e
kilpelainenj <juhomatti.kilpelainen@gmail.com>
2026-01-03 10:16:03 +0100

    Add variable declarations and assignments

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -13,7 +13,7 @@ class RuntimeError extends RuntimeException {
 
 class Interpreter implements Expr.Visitor<Object>,
                             Stmt.Visitor<Void> {
-    
+    private Environment environment = new Environment();
 
     void interpret(List<Stmt> statements) {
         try {
@@ -157,5 +157,28 @@ class Interpreter implements Expr.Visitor<Object>,
         Object value = evaluate(stmt.expression);
         System.out.println(stringify(value));
         return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+        if (stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+        }
+
+        environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
     }
 }
